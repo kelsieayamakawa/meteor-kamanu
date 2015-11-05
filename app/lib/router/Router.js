fb164e346acbe8dd759437e1557995df2f02e9d9
Kelsie Yamakawa <kayama@hawaii.edu>
2015-11-05 09:33:11 -1000

    Finished extra credit 1

--- app/lib/router/Router.js
+++ app/lib/router/Router.js
@@ -14,14 +14,9 @@ Router.route('/', {
   name: 'Home'
 });
 
-Router.route('/list', {
-  name: 'ListStuff'
+Router.route('/pueo', {
+  name: 'Pueo'
 });
-
-Router.route('/add', {
-  name: 'AddStuff'
-});
-
 
 Router.route('/stuff/:_id', {
   name: 'EditStuff',
